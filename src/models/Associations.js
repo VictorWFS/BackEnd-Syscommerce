a6c28da59a5190892e5f8967e26d7a519d2fbd26
VictorWFS <victorwfs900@gmail.com>
2025-05-13 19:23:41 -0300

    feat: Implementação do relacionamento entre models via sequelize

---
+++ src/models/Associations.js
@@ -0,0 +1,45 @@
+const User = require('./Users')
+const Product = require('./Product')
+const OrderItem = require('./OrderItem')
+const Order = require('./Order')
+const Category = require('./Category')
+const CartItem = require('./CartItem')
+const Address = require('./Address')
+
+//criando o relacionamento entre produtos -> categorias
+Category.hasMany(Product, {foreignKey: 'category_id'});
+Product.belongsTo(Category, {foreignKey: 'category_id'});
+
+//criando relacionamento entre usuário -> itens do carrinho
+User.hasMany(CartItem, {foreignKey: 'user_id'});
+CartItem.belongsTo(User, {foreignKey: 'user_id'});
+
+//criando relacionamento entre produtos -> itens do carrinho
+Product.hasMany(CartItem, {foreignKey: 'product_id'});
+CartItem.belongsTo(Product, {foreignKey: 'product_id'});
+
+//criando relacionamento entre usuários -> pedidos
+User.hasMany(Order, {foreignKey: 'user_id'});
+Order.belongsTo(User, {foreignKey: 'user_id'});
+
+//criando o relacionamento entre pedidos - itens do pedido
+Order.hasMany(OrderItem, {foreignKey: 'order_id'});
+OrderItem.belongsTo(Order, {foreignKey: 'order_id'})
+
+//criando relacionamento entre produto -> itens do pedido
+Product.hasMany(OrderItem, {foreignKey: 'product_id'});
+OrderItem.belongsTo(Product, {foreignKey: 'product_id'});
+
+//criando relacionamento entre usuário -> endereço
+User.hasMany(Address, {foreignKey: 'user_id'});
+Address.belongsTo(User, {foreignKey: 'user_id'});
+
+module.exports = {
+    User,
+    Category,
+    Product,
+    CartItem,
+    Order,
+    OrderItem,
+    Address
+};
